bd5373eaf7dbab0c723503a94bdc312133d1d03e
dadugkt856775-lab <dadugkt856775@gmail.com>
2026-02-15 19:36:53 +0530

    Add files via upload

---
+++ number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int number=25;
+    char star='*';
+    int age=22; 
+
+    printf("number = %d\n",number);
+    printf("star = %c\n",star);
+    printf("age = %d\n",age);
+    return 0;
+}
